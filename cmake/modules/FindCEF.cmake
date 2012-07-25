3969d7b1be64405faa7f56602eaa2aa358075bab
aszlig <aszlig@redmoonstudios.org>
2012-07-25 02:02:25 +0200

    cmake: Allow to use system provided CEF.
    This is our first step in tearing apart the patch, so we can build with system
    libcef and fix everything that is going to fail.
    
    This also removes dependencies of cef to wxwidgets, as this is only needed for
    desurium specific code and not used within CEF itself.

---
+++ cmake/modules/FindCEF.cmake
@@ -0,0 +1,18 @@
+IF( CEF_INCLUDE_DIR )
+    # Already in cache, be silent
+    SET( CEF_FIND_QUIETLY TRUE )
+ENDIF( CEF_INCLUDE_DIR )
+
+FIND_PATH( CEF_INCLUDE_DIR "cef_app.h" )
+
+FIND_LIBRARY( CEF_LIBRARIES
+              NAMES "cef" )
+
+# handle the QUIETLY and REQUIRED arguments and set CEF_FOUND to TRUE if
+# all listed variables are TRUE
+INCLUDE( "FindPackageHandleStandardArgs" )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( "CEF" DEFAULT_MSG
+                                   CEF_INCLUDE_DIR
+                                   CEF_LIBRARIES )
+
+MARK_AS_ADVANCED( CEF_INCLUDE_DIR CEF_LIBRARIES )
